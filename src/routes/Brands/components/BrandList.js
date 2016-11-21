d4b78d4108e6dbac7cacfc48f5a29cbfac6c0b02
chrisfitkin <cfitkin@gmail.com>
2016-11-20 16:20:19 -0800

    feat: Implement static column gridlist

--- src/routes/Brands/components/BrandList.js
+++ src/routes/Brands/components/BrandList.js
@@ -1,20 +1,45 @@
 import React, { PropTypes } from 'react'
 import Brand from './Brand'
-import '../../../styles/FlexBox.scss'
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
-const style = {
-   margin: '0 auto',
-   maxWidth: 1100
-}
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+  },
+  gridTile: {
+  },
+  container: {
+     margin: '0 auto',
+     maxWidth: 1100
+  }
+};
 
 export const BrandList = (props) => (
-  <div style={style} className={'cards-container'}>
+  <div style={styles.container}>
+    <GridList
+      style={styles.gridList}
+      cellHeight={'auto'}
+      cols={3}
+    >
       {props.brandList.map(brand =>
-        <Brand
+        <GridTile
           key={brand.id}
-          {...brand}
-        />
+          style={styles.gridTile}
+        >
+          <Brand
+            key={brand.id}
+            {...brand}
+          />
+        </GridTile>
       )}
+    </GridList>
   </div>
 )
 // {props.brands}
